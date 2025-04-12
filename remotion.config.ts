aa599ffc266a085d08710c9a54a542a7e34e5bf0
willianscurvello <123319944+willianscurvello@users.noreply.github.com>
2025-04-12 01:54:13 -0300

    Update remotion.config.ts

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,6 +1,6 @@
 // remotion.config.ts
 import { Config } from "@remotion/cli/config";
 
+Config.setVideoImageFormat("jpeg"); // substitui o antigo setImageFormat()
 Config.setCodec("h264");
-Config.setImageFormat("jpeg"); // ou png, se preferir
 Config.setTimeoutInMilliseconds(120000); // opcional
