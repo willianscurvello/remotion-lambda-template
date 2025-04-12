7a7f8900c92f42f13f2132e411035dc966058f1d
willianscurvello <123319944+willianscurvello@users.noreply.github.com>
2025-04-12 00:15:21 -0300

    Create MyVideo.tsx

---
+++ src/MyVideo.tsx
@@ -0,0 +1,7 @@
+export const MyVideo: React.FC = () => {
+  return (
+    <div style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
+      <h1 style={{ fontSize: 60 }}>Olá, mundo!</h1>
+    </div>
+  );
+};
